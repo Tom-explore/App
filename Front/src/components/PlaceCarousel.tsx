72ccfb44a32d700d4d1b3f221efc34ee71c81dfc
gpocil <pocaussarieu@gmail.com>
2024-12-05 11:04:10 +0100

    propre thomas

--- Front/src/components/PlaceCarousel.tsx
+++ Front/src/components/PlaceCarousel.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, FreeMode } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import 'swiper/css/free-mode';
 import './PlaceCarousel.css';
 import PlaceCard from './PlaceCard';
 import { Place } from '../types/PlacesInterfaces';
-
 
 interface PlaceCarouselProps {
     title: string;
@@ -32,11 +32,12 @@ const PlaceCarousel: React.FC<PlaceCarouselProps> = ({ title, places, loading })
         <div className="place-carousel">
             <h2>{title}</h2>
             <Swiper
-                modules={[Navigation, Pagination]}
+                modules={[Navigation, Pagination, FreeMode]}
                 spaceBetween={16}
                 slidesPerView={6}
                 navigation
                 pagination={{ clickable: true, dynamicBullets: true }}
+                freeMode={true} // Active le mode libre
                 breakpoints={{
                     20: { slidesPerView: 1 },
                     320: { slidesPerView: 2 },
@@ -46,13 +47,14 @@ const PlaceCarousel: React.FC<PlaceCarouselProps> = ({ title, places, loading })
                     1200: { slidesPerView: 6 },
                 }}
                 loop={false}
+                draggable={true}
             >
                 {loading && places.length === 0
                     ? renderSkeletons(4).map((skeleton, index) => (
                         <SwiperSlide key={`skeleton-${index}`}>{skeleton}</SwiperSlide>
                     ))
-                    : places.map((place) => (
-                        <SwiperSlide key={`place-${place.id}`}>
+                    : places.map((place, index) => (
+                        <SwiperSlide key={`place-${place.id}-${index}`}>
                             <PlaceCard place={place} />
                         </SwiperSlide>
                     ))}
